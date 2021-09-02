311508375674b2246d23e3e09a4fd1c38bd908f9
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2021-09-02 19:47:21 +0200

    Add basic player information and states

---
+++ project/player/Player.gd
@@ -0,0 +1,8 @@
+extends KinematicBody
+class_name Player
+
+export var gravity = Vector3.DOWN * 10
+export var speed = 4
+export var rotation_speed = 0.85
+
+var velocity = Vector3.ZERO
--- project/player/PlayerState.gd
+++ project/player/PlayerState.gd
@@ -1,2 +1,13 @@
 extends State
 class_name PlayerState, "res://assets/editor/icons/state-machine/state.png"
+
+onready var player : Player = self._state_machine.get_parent()
+
+# export(NodePath) var mesh_nodepath
+# onready var mesh : Node = get_node(mesh_nodepath)
+
+
+func physics_process(delta: float) -> void:
+	player.velocity += player.gravity * delta
+	# get_input(delta)
+	player.velocity = player.move_and_slide(player.velocity, Vector3.UP)
